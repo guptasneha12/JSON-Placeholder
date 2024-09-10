557bb923194ea667770dc5d031f31544e5343fb4
Sneha Gupta <guptasneha2003.sg@gmail.com>
2024-09-11 00:39:50 +0530

    first-commit

---
+++ src/components/PostList.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './PostList.css';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("Error fetching data");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="post-list-container">
+      <h2 className="title">Posts</h2>
+      <ul className="post-list">
+        {posts.map((post) => (
+          <li key={post.id} className="post-card">
+            <h3 className="post-title">{post.title}</h3>
+            <p className="post-body">{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
